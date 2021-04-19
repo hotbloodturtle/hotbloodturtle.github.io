f9720f029022d55087c42a3edf23c150a2499a87
HaeSeok Kang <hotbloodturtle@gmail.com>
2021-04-19 15:20:45 +0900

    add - index.js , App.js
    n/a

---
+++ js/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+  const div = document.createElement("div");
+  div.textContent = "welcome home";
+  return div;
+};
+
+export default App;
--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log("hi");
+import App from "./App.js";
+
+const root = document.getElementById("root");
+
+window.addEventListener("DOMContentLoaded", () => {
+  const app = App();
+  root.appendChild(app);
+});
